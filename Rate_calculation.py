b04bdd6d850ac1dd62a2c5a3e88695c00925c674
Ludvig Lindblad <65110467+LudvigLindblad@users.noreply.github.com>
2020-05-14 16:19:10 +0200

    Update Rate_calculation.py

--- Rate_calculation.py
+++ Rate_calculation.py
@@ -28,15 +28,13 @@ v0=220e3;
 vesc=550e3;
 vobs=244e3;
 
-mu=(ct.me*ct.mchi)/(ct.me+ct.mchi);
-
-R_1=[ct.a, 0, 0];      #Nearest-neighbor vectors
+#Nearest-neighbor vectors
+R_1=[ct.a, 0, 0];      
 R_2=[-ct.a/2, 3**(1/2)*ct.a/2, 0];
 R_3=[-ct.a/2, -3**(1/2)*ct.a/2, 0];
 
-
-
-def fl(lx, ly, lz):  #Phase factor
+#Phase factor
+def fl(lx, ly, lz):
     l=[lx, ly, lz];
 
     out=mp.exp(1j*mp.fdot(l, R_1))+mp.exp(1j*mp.fdot(l, R_2))+mp.exp(1j*mp.fdot(l, R_3));
@@ -44,7 +42,6 @@ def fl(lx, ly, lz):  #Phase factor
 
 
 #Momentum space orbitals (normalized)
-
 def w2pz(kx, ky, kz):
     return ct.nrm_pz*ct.a0**(5/2)*(kz/ct.hbar)/((ct.a0*mp.norm([kx, ky, kz])/ct.hbar)**2+(ct.Zeff2pz/2)**2)**3;
 
@@ -59,7 +56,6 @@ def w2s(kx, ky, kz):
 
 
 #bloch subfunctions for sigma orbitals
-
 def fi_As(lx, ly, kx, ky, kz):
     return w2s(kx, ky, kz);
 
@@ -78,13 +74,9 @@ def fi_Bx(lx, ly, kx, ky, kz):
 def fi_By(lx, ly, kx, ky, kz):
     return (mp.exp(1j*mp.fdot([lx+kx/ct.hbar, ly+ky/ct.hbar, kz/ct.hbar], R_1))+mp.exp(1j*mp.fdot([lx+kx/ct.hbar, ly+ky/ct.hbar, kz/ct.hbar], R_2))+mp.exp(1j*mp.fdot([lx+kx/ct.hbar, ly+ky/ct.hbar, kz/ct.hbar], R_3)))*w2py(kx, ky, kz);
 
-
-
-
-#returns lambda function for normalized pi/sigma electron wavefunction
-
 table=loadtables.Table(); 
 
+#returns lambda function for normalized pi/sigma electron wavefunction
 def createP_pi(lx, ly):
     fle=fl(lx, ly, 0);
     phi_l=-mp.atan(mp.im(fle)/mp.re(fle));
@@ -99,7 +91,6 @@ def createP_pi(lx, ly):
 
     nrm=mp.sqrt(1/(2+2*s/(3**(1/2))*(mp.cos(phi_l+mp.fdot([lx, ly, 0], R_1))+mp.cos(phi_l+mp.fdot([lx, ly, 0], R_2))+mp.cos(phi_l+mp.fdot([lx, ly, 0], R_3)))+2*s2*(mp.cos(mp.fdot([lx, ly, 0], R_12))+mp.cos(mp.fdot([lx, ly, 0], R_23))+mp.cos(mp.fdot([lx, ly, 0], R_31)))));
     
-
     return lambda kx, ky, kz: f(kx, ky, kz)*nrm;
 
 def createP_sigma(lx, ly, band): 
@@ -109,37 +100,33 @@ def createP_sigma(lx, ly, band):
 
     f=lambda kx, ky, kz: C[0]*fi_As(lx, ly, kx, ky, kz)+C[1]*fi_Ax(lx, ly, kx, ky, kz)+C[2]*fi_Ay(lx, ly, kx, ky, kz)+C[3]*fi_Bs(lx, ly, kx, ky, kz)+C[4]*fi_Bx(lx, ly, kx, ky, kz)+C[5]*fi_By(lx, ly, kx, ky, kz);
     
-    
-    nrm3=1/mp.sqrt(mp.conj(C[0])*C[0]+mp.conj(C[1])*C[1]+mp.conj(C[2])*C[2]+3*(mp.conj(C[3])*C[3]+mp.conj(C[4])*C[4]+mp.conj(C[5])*C[5])+2*mp.re((mp.conj(C[0])*C[3]*pss+mp.conj(C[0])*C[4]*psx+mp.conj(C[0])*C[5]*psy+mp.conj(C[1])*C[3]*psx+mp.conj(C[1])*C[4]*pxx+mp.conj(C[1])*C[5]*pxy+mp.conj(C[2])*C[3]*psy+mp.conj(C[2])*C[4]*pxy+mp.conj(C[2])*C[5]*pyy)*(mp.exp(1j*mp.fdot([lx, ly, 0], R_1))+mp.exp(1j*mp.fdot([lx, ly, 0], R_2))+mp.exp(1j*mp.fdot([lx, ly, 0], R_3)))));
+    nrm=1/mp.sqrt(mp.conj(C[0])*C[0]+mp.conj(C[1])*C[1]+mp.conj(C[2])*C[2]+3*(mp.conj(C[3])*C[3]+mp.conj(C[4])*C[4]+mp.conj(C[5])*C[5])+2*mp.re((mp.conj(C[0])*C[3]*pss+mp.conj(C[0])*C[4]*psx+mp.conj(C[0])*C[5]*psy+mp.conj(C[1])*C[3]*psx+mp.conj(C[1])*C[4]*pxx+mp.conj(C[1])*C[5]*pxy+mp.conj(C[2])*C[3]*psy+mp.conj(C[2])*C[4]*pxy+mp.conj(C[2])*C[5]*pyy)*(mp.exp(1j*mp.fdot([lx, ly, 0], R_1))+mp.exp(1j*mp.fdot([lx, ly, 0], R_2))+mp.exp(1j*mp.fdot([lx, ly, 0], R_3)))));
    
-    return lambda kx, ky, kz: f(kx, ky, kz)*nrm3;  
-
-
+    return lambda kx, ky, kz: f(kx, ky, kz)*nrm;  
 
 #binding energy functions
 
 def E_pi_minus(lx, ly):
     fle=float(mp.re(mp.sqrt(mp.conj(fl(lx, ly, 0)*fl(lx, ly, 0)))));
-    return -1*t*fle/(1+s*fle);     #Returns negative value
+    return -1*t*fle/(1+s*fle);
 
 def E_sigma(lx, ly, band):
     E=table.Ematrix(lx, ly, band);
-    return -1*ct.q*E;   #Returns negative value
+    return -1*ct.q*E;
 
+#form factor of interaction
 def F_DM(qr, fdm):
     # 3 cases
     if fdm==1:
         return ct.sigma_e;  
     elif fdm==2:
-        return ct.sigma_e*qr;    # sigma_e not needed, the scale is arbitrary.         !!!!!  Fabian formulera om?
+        return ct.sigma_e*qr;
     elif fdm==3:
         return ct.sigma_e*qr**2;
     else:
         raise Exception("fdm error");       
         return 0;
-
-#directly from Riccardo
-
+    
 class calculatormk3():
     lastlx=-1;
     lastly=-1;
@@ -154,7 +141,6 @@ class calculatormk3():
 
     def __init__(self, method):
         self.method=method;
- 
 
     def vmin(self, q, kf, E, local_mchi):  
         out=(1/q)*(kf**2/(2*ct.me)+E+ct.fi)+q/(2*local_mchi);
@@ -190,7 +176,7 @@ class calculatormk3():
 
         return out;
 
-     # q momentum integral
+    #q momentum integral
     def dR(self, lx, ly, kr, kt, kp, qmin, qmax, band, fdm, local_mchi):
         if band=="pi":
             if not self.lastlx==lx or not self.lastly==ly:
@@ -221,7 +207,6 @@ class calculatormk3():
         integrand=self.dRdEe_noint(lx, ly, band, fdm, mchi_index);
 
         return lambda kr: (1/ct.hbar)**3*((kr)**3)/(32*mp.pi**4)*mint.integrate(lambda kt, kp: integrand(kr, kt, kp), [0, mp.pi], [0, 2*mp.pi], 0, self.method);
-
     
     #m_chi index only used in pi band. set to 0 to disable m_chi looping
     def dRdEe_noint(self, lx, ly, band, fdm, mchi_index):
